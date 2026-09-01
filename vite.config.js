365370e2f26c0253d608b92776000d6cdf638d66
Hachiware <2862297923@qq.com>
2026-09-01 18:03:54 +0800

    update package config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/PDDA-Assistant/',
+  base: '/vehicle-concept-generation/',
   build: {
     rollupOptions: {
       input: {
